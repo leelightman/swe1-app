326ffb060719e065ff8e66290e27d2555b250b31
Lynn <leelightman@gmail.com>
2020-10-15 20:13:33 -0400

    Update views.py

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -26,7 +26,8 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
-    
+
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
